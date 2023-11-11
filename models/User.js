92148737bcec9dd337c10b9293a44f88add49077
ingridmidia <ingridmidia@gmail.com>
2023-11-11 10:37:11 -0600

    initial files and models

---
+++ models/User.js
@@ -0,0 +1,27 @@
+const { Schema, model } = require('mongoose');
+
+// Schema to create User model
+const userSchema = new Schema(
+    {
+        username: { type: String, unique: true, required: true, trim: true },
+        email: { type: String, unique: true, required: true, match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address'] },
+        thoughts: [{ type: Schema.Types.ObjectId, ref: "Thought" }],
+        friends: [{ type: Schema.Types.ObjectId, ref: "User" }]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+);
+
+// Create a virtual property `friendCount` that gets the amount of friends per user
+postSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+});
+
+// Initialize User model
+const User = model("user", userSchema);
+
+module.exports = User;

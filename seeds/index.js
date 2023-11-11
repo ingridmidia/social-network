4098e4256eb8355f2812f05e39e75a85079815d4
ingridmidia <ingridmidia@gmail.com>
2023-11-11 16:50:48 -0600

    finished seed data

--- seeds/index.js
+++ seeds/index.js
@@ -5,22 +5,35 @@ const userSeedData = require('./userSeedData.json');
 const thoughtSeedData = require('./thoughtSeedData.json');
 
 connection.once("open", async function () {
-    const userCollection = await User.find({}).exec();
-    if (userCollection.length === 0) {
-        await User.insertMany(userSeedData);
-        console.log('Users inserted');
-    } else {
-        console.log('Users already populated');
-    }
-    
-    const thoughtCollection = await Thought.find({}).exec();
-    if (thoughtCollection.length === 0) {
-        await Thought.insertMany(thoughtSeedData);
-        console.log('Thoughts inserted');
-    } else {
-        console.log('Thougts already populated');
-    }
-    
+
+    // Create users
+    await User.insertMany(userSeedData);
+
+    // Create thoughts
+    await Thought.insertMany(thoughtSeedData);
+
+    // Find users to associate thoughts and friends
+    const maggie = await User.findOne({ "username": "maggie" });
+    const alice = await User.findOne({ "username": "alice" });
+    const nucita = await User.findOne({ "username": "nucita" });
+
+    // Find thoughts to be associated
+    const maggieThought = await Thought.findOne({ "username": "maggie" });
+    const aliceThought = await Thought.findOne({ "username": "alice" });
+    const nucitaThought = await Thought.findOne({ "username": "nucita" });
+
+    // Add thoughts and friend to each user
+    maggie.thoughts.push(maggieThought._id);
+    maggie.friends.push(alice._id);
+    await maggie.save();
+
+    alice.thoughts.push(aliceThought._id);
+    alice.friends.push(nucita._id);
+    await alice.save();
+
+    nucita.thoughts.push(nucitaThought._id);
+    nucita.friends.push(maggie._id);
+    await nucita.save();
+
     process.exit(0);
 });
-
